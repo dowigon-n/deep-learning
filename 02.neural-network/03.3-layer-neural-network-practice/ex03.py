7b76a13a4032b56c84036c54d768b393753f1a22
bithorang030 <bitxecreator030@naver.com>
2020-11-24 11:09:23 +0900

    신경망 신호전달 연습 #1

---
+++ 02.neural-network/03.3-layer-neural-network-practice/ex03.py
@@ -0,0 +1,11 @@
+# 3층 신경망 신호 전달 구현3 : 은닉 2층 전달
+import ...
+
+try:
+    sys.path.append(os.path.join(os.getcwd()))
+    from ex02 import z1
+except ImportError:
+    print('Library Module Can Not Found')
+
+print('\n= 신호전달 구현3: 은닉 2층 전달 =====================')
+print(f'a1 dimension: {z1.shape}') # 3 Vector
